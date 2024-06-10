4dce199d13ae27755a2ea5f4c247e8162cad2890
zhou dao <1422024869@qq.com>
2024-06-10 23:41:17 +0800

    12
    12

--- software-testing/src/views/twelve/SingleTest.vue
+++ software-testing/src/views/twelve/SingleTest.vue
@@ -1,11 +1,75 @@
 <script setup>
+import {ref,computed} from 'vue'
+import {sales} from "@/utils/sales.js";
+const formInput = ref({
+  A:'',
+  B:'',
+  C:'',
+})
+const expectation = computed(()=>{
+  return sales(formInput.value)
+})
+const loading = ref(false)
+const actual = ref('')
+const doTest = () =>{
+  actual.value = sales(formInput.value)
+}
 
 </script>
 
 <template>
-
+  <div class="single-case">
+    <el-form
+        class="single-form"
+        label-width="400px"
+        :model="formInput"
+    >
+      <el-form-item label="年销售额">
+        <el-input v-model.number="formInput.A"></el-input>
+      </el-form-item>
+      <el-form-item label="现金到账率">
+        <el-input v-model="formInput.B"></el-input>
+      </el-form-item>
+      <el-form-item label="请假天数">
+        <el-input v-model.number="formInput.C"></el-input>
+      </el-form-item>
+      <el-form-item label="程序预期输出">
+        <el-input v-model="expectation.money" disabled></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button
+        class="main-button"
+        type="success"
+        plain
+        @click="doTest"
+        :loading="loading"
+    >
+      进行测试
+    </el-button>
+    <div class="actual">
+      <span>实际输出：{{actual}}</span>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="less">
+
+
+.single-case{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+.single-form{
+  width: 800px;
+  transform: translate(-20%,0);
+}
+.main-button{
+  margin-bottom: 50px;
+}
+.actual{
+  margin: 0 auto;
+}
 
 </style>
--- software-testing/src/views/twelve/SystemTest.vue
+++ software-testing/src/views/twelve/SystemTest.vue
@@ -1,11 +1,61 @@
 <script setup>
+import test1 from '@/mock/twelve/test1.json'
+import { ref, computed } from 'vue'
+import { sales } from "@/utils/sales.js";
+
+const system = ref(test1)
+
+const systemData = computed(() => {
+    return system.value
+})
+console.log(system.value)
+
+const loading = ref(false)
+
+const doTest = () => {
+    loading.value = true
+    system.value.forEach(item => {
+        item.actual = sales({ A: item.V, B: item.R, C: item.D })
+        item.state = (item.actual.money === item.ExpectedResult ) ? '测试通过' : '测试不通过'
+    })
+    loading.value = false
+}
+
+const reset = () => {
+    system.value.forEach(item => {
+        item.actual = ''
+        item.state = ''
+    })
+}
 
 </script>
 
 <template>
-
+    <div class="main-header">
+        <div class="button-group">
+            <el-button class="main-button" type="success" plain @click="doTest" :loading="loading">进行测试<i
+                    class="el-icon-upload el-icon--right"></i></el-button>
+            <el-button @click="reset" class="reset-button" type="warning" plain>重置</el-button>
+        </div>
+    </div>
+    <el-table :data="systemData" border style="width: 100%">
+        <el-table-column prop="Use Case ID" label="Use Case ID" min-width="100" />
+        <el-table-column prop="V" label="年销售额" min-width="100" />
+        <el-table-column prop="R" label="现金到帐率" min-width="100" />
+        <el-table-column prop="D" label="请假天数" min-width="100" />
+        <el-table-column prop="Coverage Combination" label="覆盖组合" min-width="100" />
+        <el-table-column prop="Path" label="路径" min-width="100" />
+        <el-table-column prop="ExpectedResult" label="预期结果" min-width="100" />
+        <el-table-column prop="actual.money" label="实际佣金" min-width="100" />
+        <el-table-column prop="state" label="测试结果" min-width="100" />
+    </el-table>
 </template>
 
 <style scoped>
-
+.main-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
 </style>
